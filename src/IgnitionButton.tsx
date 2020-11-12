65e086421e77b542f9cfcefc9ad04487c5f82c83
Anna Le <Anna.Le@LibertyMutual.com>
2020-11-12 10:58:25 -0500

    Changed alerts into modals so they look nicer

--- src/IgnitionButton.tsx
+++ src/IgnitionButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Modal } from 'react-bootstrap';
 
 interface engineProp {
 	engine: string;
@@ -7,6 +8,24 @@ interface engineProp {
 const IgnitionButton = ({ engine }: engineProp): JSX.Element => {
 	const [start, setStart] = React.useState(false);
 	const [stop, setStop] = React.useState(false);
+	const [modal, setModal] = React.useState(false);
+	const [engineState, setEngineState] = React.useState("");
+
+	const handleStart = () => {
+		setStart(true);
+		setStop(false);
+		setModal(true)
+		setEngineState("STARTED")
+	}
+
+	const handleStop = () => {
+		setStart(false);
+		setStop(true);
+		setModal(true);
+		setEngineState("STOPPED")
+	}
+
+	const closeModal = () => setModal(false);
 
 	return (
 		<div>
@@ -14,11 +33,7 @@ const IgnitionButton = ({ engine }: engineProp): JSX.Element => {
 				type='radio'
 				value='Start'
 				checked={start}
-				onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => {
-					alert(`ALERT! ${engine} ENGINE STARTED.`);
-					setStart(true);
-					setStop(false);
-				}}
+				onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => {handleStart()}}
 			/>{' '}
 			<label className="button-text button-start">Start Ignition</label>
 			<br />
@@ -26,13 +41,13 @@ const IgnitionButton = ({ engine }: engineProp): JSX.Element => {
 				type='radio'
 				value='Stop'
 				checked={stop}
-				onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => {
-					alert(`ALERT! ${engine} ENGINE STOPPED.`);
-					setStart(false);
-					setStop(true);
-				}}
+				onChange={(ev: React.ChangeEvent<HTMLInputElement>): void => {handleStop()}}
 			/>{' '}
 			<label className="button-text button-stop">Stop Ignition</label>
+
+			<Modal show={modal} onHide={closeModal} centered>
+				<Modal.Body>ALERT! YOU JUST {engineState} THE <b>{engine}</b> ENGINE.</Modal.Body>
+			</Modal>
 		</div>
 	);
 };
